8743d7bbb0df3af8a09803bc20544841a2ae5f7b
Gomes-ltda <migueloliveiragomes45@gmail.com>
2026-09-20 11:38:52 -0400

    Permitir execução interna de preparação de abordagem

--- agent.py
+++ agent.py
@@ -13,7 +13,7 @@ from memory import (
 
 ACOES_PERMITIDAS = {
     "aguardar", "pesquisar", "analisar", "criar_oferta",
-    "criar_proposta", "criar_conteudo", "testar_estrategia"
+    "criar_proposta", "criar_conteudo", "preparar_abordagem", "testar_estrategia"
 }
 
 MAX_TAREFAS_POR_CICLO = 3
